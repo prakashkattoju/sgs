d7a1fb2a6724103f3bdd94ef86376cc49f141308
Prakash UI <prakash.kattoju@gmail.com>
2026-02-12 19:23:18 +0530

    12-02-26 edit page

--- src/pages/Temp.jsx
+++ src/pages/Temp.jsx
@@ -74,8 +74,8 @@ export default function Temp() {
 
     const [editStatus, setEditStatus] = useState(0)
     const [filteredData, setFilteredData] = useState([]);
-    const [complete, setComplete] = useState("")
-    const [pending, setPending] = useState("")
+    const [complete, setComplete] = useState(0)
+    const [pending, setPending] = useState(0)
 
 
     useEffect(() => {
@@ -546,9 +546,9 @@ export default function Temp() {
                                             return (<tr key={item.item_id} className={item.item_id === itemData?.item_id ? 'active' : ''}>
                                                 <td>
                                                     <div className='d-flex justify-content-between align-items-center gap-1'>
-                                                        <div>{item.item}<small><br />Packing: {item.unit} &nbsp;&nbsp;&middot;&nbsp;&nbsp;Price: {item.price}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.category && item.scategory && `${item.category} | ${item.scategory}`}</small></div>
+                                                        <div>{item.item}<small><br />Packing: {item.unit}&nbsp;&nbsp;&middot;&nbsp;&nbsp;Price: {item.price}<br/ >{item.category && item.scategory && `${item.category} | ${item.scategory}`}</small></div>
 
-                                                        <div className={`d-flex flex-column align-items-center justify-content-between gap-1`}>
+                                                        <div className={`d-flex align-items-center justify-content-between gap-3`}>
                                                             {/* Edit Record */}
                                                             <button onClick={() => handleEditItem(item)} className="icon-btn-cart small add" type="button"><svg xmlns="http://www.w3.org/2000/svg" height="15px" viewBox="0 -960 960 960" width="15px" fill="currentColor"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" /></svg></button>
                                                             {/* Delate Record */}
