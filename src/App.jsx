01991e241234d8d78898199884a09e9dc8611a09
Prakash UI <prakash.kattoju@gmail.com>
2026-05-14 15:09:31 +0530

    your changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,8 @@ import { useDispatch } from 'react-redux';
 import { getToken } from './util/Cookies'
 import { checkAndRemoveExpiredToken, fetchUserRole } from "./util/authUtils";
 import ProtectedRoute from './components/ProtectedRoute';
-import Bill from "./pages/Bill";
-import Account from "./pages/Account";
-import OrderDetails from "./pages/OrderDetails";
-import SearchItems from "./pages/SearchItems";
-import OrderAgain from "./pages/OrderAgain";
 import LoginAdmin from "./pages/LoginAdmin";
 import Temp from "./pages/Temp";
-import Orders from "./pages/Orders";
 
 function App() {
   const dispatch = useDispatch();
@@ -37,19 +31,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={isLoggedIn ? <ProtectedRoute allowedRoles={["user"]}><Home /></ProtectedRoute> : <Login />} />
-
-      {/* <Route path="/sgs" element={<Temp />} /> */}
-
-      <Route path="/sgs" element={isLoggedIn ? <ProtectedRoute allowedRoles={["Admin", "Emp"]}><Temp /></ProtectedRoute> : <LoginAdmin/>}  />
-
-      <Route path="/search-items" element={<ProtectedRoute allowedRoles={["user"]}><SearchItems /></ProtectedRoute>} />
-      <Route path="/order-again" element={<ProtectedRoute allowedRoles={["user"]}><OrderAgain /></ProtectedRoute>} />
-      <Route path="/cart" element={<ProtectedRoute allowedRoles={["user"]}><Cart /></ProtectedRoute>} />
-      <Route path="/bill" element={<ProtectedRoute allowedRoles={["user"]}><Bill /> </ProtectedRoute>} />
-      <Route path="/account" element={<ProtectedRoute allowedRoles={["user"]}><Account /> </ProtectedRoute>} />
-      <Route path="/account/orders" element={<ProtectedRoute allowedRoles={["user"]}><Orders /> </ProtectedRoute>} />
-      <Route path="/order-details" element={<ProtectedRoute allowedRoles={["user"]}><OrderDetails /> </ProtectedRoute>} />
+      <Route path="/" element={isLoggedIn ? <ProtectedRoute allowedRoles={["Admin", "Emp"]}><Temp /></ProtectedRoute> : <LoginAdmin/>}  />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
